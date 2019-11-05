5e1977f80cf320efd6812c36f8e3a733e43309ad
Burak Selim Şenyurt <ahch-to@Buraks-Mac-mini.local>
2019-11-05 22:37:27 +0300

    tüm oyuncu listesini stream tipinden döndüren metod eklendi

--- No 02 - Talking MongoDb with GoLang/src/playerserver/main.go
+++ No 02 - Talking MongoDb with GoLang/src/playerserver/main.go
@@ -11,6 +11,7 @@ import (
 
 	playerpb "gRPC-sample/proto"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
@@ -18,7 +19,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-/* proto'dan otomatik üretilen player.pb.go içerisindeki RegisterPlayerServiceServer metoduna bakın.
+/* proto'dan otomatik üretilen player.pb.go içerisindeki RegisterPlayerServiceServer metoduna bir bakın.
 Pointer olarak gelen grpc server nesnesi ikinci parametre olarak gelen tipi register etmek için kullanılır.
 Bir nevi interface üzerinden enjekte işlemi yaptığımızı düşünebilir miyiz?
 */
@@ -85,7 +86,7 @@ func main() {
 	fmt.Println("GoodBye Crow")
 }
 
-/* Protobuf mesajlarında taşınan serileşmiş içeriği nesnel olarak ele alacağımız struct */
+/* Protobuf mesajlarında taşınan serileşmiş içeriği nesnel olarak ele alacağımı struct */
 type Player struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty"` // MongoDB tarafındaki ObjectId değerini taşır
 	PlayerID string             `bson:"player_id"`
@@ -94,14 +95,14 @@ type Player struct {
 	Bio      string             `bson:"bio"`
 }
 
-/*PlayerServiceServer'ın uygulanması gereken metodları. Yani servis sözleşmesinin tüm operasyonları*/
+/* PlayerServiceServer'ın uygulanması gereken metodlarını. Yani servis sözleşmesinin tüm operasyonları
+ */
 
 // Yeni bir oyuncu eklemek için kullanacağımız fonksiyon
 func (srv *PlayerServiceServer) AddPlayer(ctx context.Context, req *playerpb.AddPlayerReq) (*playerpb.AddPlayerRes, error) {
-	payload := req.GetPlyr() // GetPlyr (GetPlayer değil. O servis metodumuz) fonksiyonu ile request üzerinden gelen player içeriği çekilir
+	payload := req.GetPlyr() // GetPlyr (GetPlayer değil o servis metodumuz) fonksiyonu ile request üzerinden gelen player içeriği çekilir
 
 	// İçerik ile gelen alan değerleri player struct nesnesini oluşturmak için kullanılır
-	// GetPlayer[Özellik Adı] metodları derlenmiş protobuff dosyasında yer alır. Bir kontrol edin.
 	player := Player{
 		PlayerID: payload.GetPlayerId(),
 		Fullname: payload.GetFullname(),
@@ -109,12 +110,12 @@ func (srv *PlayerServiceServer) AddPlayer(ctx context.Context, req *playerpb.Add
 		Bio:      payload.GetBio(),
 	}
 
-	// player nesnesi mongodb veritabanındaki koleksiyona eklenir
+	// player nesnesi mongodb veritabanındaki koleksiyona kayıt edilir
 	result, err := playerCollection.InsertOne(mongoContext, player)
 
-	// InsertOne operasyonunun sonucuna bakılır.
+	// bir problem oluştuysa
 	if err != nil {
-		// gRPC hatası varsa döndürülür
+		// gRPC hatası döndürülür
 		return nil, status.Errorf(
 			codes.Internal,
 			fmt.Sprintf("Bir hata oluştu : %v", err),
@@ -123,8 +124,8 @@ func (srv *PlayerServiceServer) AddPlayer(ctx context.Context, req *playerpb.Add
 
 	// Hata oluşmadıysa koleksiyona eklenen yeni doküman
 	// üretilen ObjectID değeri de atanarak geri döndürülür
-	objectID := result.InsertedID.(primitive.ObjectID)
-	payload.Id = objectID.Hex()
+	objectId := result.InsertedID.(primitive.ObjectID)
+	payload.Id = objectId.Hex()
 	return &playerpb.AddPlayerRes{Plyr: payload}, nil
 }
 
@@ -140,6 +141,39 @@ func (srv *PlayerServiceServer) GetPlayer(ctx context.Context, req *playerpb.Get
 	return nil, nil
 }
 
-func (srv *PlayerServiceServer) GetPlayerList(ctx context.Context, req *playerpb.GetPlayerListReq) (*playerpb.GetPlayerListRes, error) {
-	return nil, nil
+// Tüm oyuncu listesini stream olarak dönen metod
+func (srv *PlayerServiceServer) GetPlayerList(req *playerpb.GetPlayerListReq, stream playerpb.PlayerService_GetPlayerListServer) error {
+
+	currentPlayer := &Player{}
+
+	// Find metodu veri üzerinden hareket edebileceğimiz bir Cursor nesnesi döndürür
+	// bu cursor nesnesi sayesinde istemciye tüm oyuncu listesini bir seferde göndermek yerine
+	// birer birer gönderme şansına sahip olacağız
+	// Bu nedenle sunucu bazlı bir streamin stratejimiz var
+	cursor, err := playerCollection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return status.Errorf(codes.Internal, fmt.Sprint("Bilinmeyen hata oluştu"))
+	}
+
+	// metod işleyişini tamamladığında cursor nesnesini kapatacak çağrıyı tanımlıyoruz
+	defer cursor.Close(context.Background())
+
+	// iterasyona başlanır ve Next true döndüğü sürece devam eder
+	// yani okunacak mongodb dokümana kalmayana dek
+	for cursor.Next(context.Background()) {
+		// cursor verisini currentPlayer nesnesine açıyoruz
+		cursor.Decode(currentPlayer)
+		// istemciye mongodb'den gelen güncel oyuncu bilgisinden yararlanarak cevap dönüyoruz
+		stream.Send(&playerpb.GetPlayerListRes{
+			Plyr: &playerpb.Player{
+				Id:       currentPlayer.ID.Hex(),
+				PlayerId: currentPlayer.PlayerID,
+				Fullname: currentPlayer.Fullname,
+				Position: currentPlayer.Position,
+				Bio:      currentPlayer.Bio,
+			},
+		})
+	}
+
+	return nil
 }
