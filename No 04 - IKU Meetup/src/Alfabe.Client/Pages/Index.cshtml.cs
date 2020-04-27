931e85ce030ec47a800535bcbdb2c1895bebfddf
Burak Selim Senyurt (Dogus Teknoloji) <BurakSelim.Senyurt@d-teknoloji.com.tr>
2020-04-27 23:47:25 +0300

    Delete operasyonu eklendi

--- No 04 - IKU Meetup/src/Alfabe.Client/Pages/Index.cshtml.cs
+++ No 04 - IKU Meetup/src/Alfabe.Client/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using Alfabe.Core;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -11,21 +12,29 @@ namespace Alfabe.Client.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly HttpClient _client;
         public IList<Team> Teams { get; private set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _client = new HttpClient();
         }
 
         public async Task OnGetAsync()
         {
-            HttpClient client = new HttpClient();
             var url = "http://localhost:5000/nba/api/teams";
-            HttpResponseMessage response = await client.GetAsync(url);
+            HttpResponseMessage response = await _client.GetAsync(url);
             response.EnsureSuccessStatusCode();
             var resp = await response.Content.ReadAsStringAsync();
             Teams = JsonConvert.DeserializeObject<List<Team>>(resp);
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(string id)
+        {
+            var url = $"http://localhost:5000/nba/api/teams/{id}";
+            HttpResponseMessage response = await _client.DeleteAsync(url);
+            return RedirectToPage();
         }
     }
 }
