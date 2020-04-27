0029d06ce31f4fb36d4c894ee566677df11246ab
Burak Selim Senyurt (Dogus Teknoloji) <BurakSelim.Senyurt@d-teknoloji.com.tr>
2020-04-27 10:20:03 +0300

    Örnek bir WebAPI eklendi.

---
+++ No 03 - HowTo Monitoring/src/ApiFarm/DemographicAPI/Controller/PhoneCodesController.cs
@@ -0,0 +1,22 @@
+﻿using DemographicAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemographicAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhoneCodesController : ControllerBase
+    {
+        [HttpGet("{code:length(3)}")]
+        public ActionResult<CountryCode> Get(string code)
+        {
+            var countryCode = new CountryCode
+            {
+                Abbrevation = "TR",
+                Name = "Türkiye",
+                PhoneCode = "90"
+            };
+            return countryCode;
+        }
+    }
+}
---
+++ No 03 - HowTo Monitoring/src/ApiFarm/DemographicAPI/Model/CountryCode.cs
@@ -0,0 +1,9 @@
+﻿namespace DemographicAPI.Model
+{
+    public class CountryCode
+    {
+        public string Name { get; set; }
+        public string Abbrevation { get; set; }
+        public string PhoneCode { get; set; }
+    }
+}
