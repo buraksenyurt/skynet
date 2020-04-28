f9829118e37a1db90be82af07871b6d75b96a348
Burak Selim Senyurt (Dogus Teknoloji) <BurakSelim.Senyurt@d-teknoloji.com.tr>
2020-04-28 15:12:28 +0300

    Create sayfası eklendi

---
+++ No 04 - IKU Meetup/src/Alfabe.Client/Pages/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Alfabe.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Alfabe.Client.Pages
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Team Team { get; set; }
+
+        private readonly HttpClient _client;
+
+        public CreateModel()
+        {
+            _client = new HttpClient();
+        }
+        //public void OnGet()
+        //{
+
+        //}
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var url = "http://localhost:5000/nba/api/teams";
+            var json = JsonConvert.SerializeObject(Team);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            await _client.PostAsync(url, data);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
