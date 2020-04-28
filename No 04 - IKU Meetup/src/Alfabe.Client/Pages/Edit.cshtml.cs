d11bab26b6bbd3d2b2d4338943155b191ca9f9a7
Burak Selim Senyurt (Dogus Teknoloji) <BurakSelim.Senyurt@d-teknoloji.com.tr>
2020-04-28 17:02:53 +0300

    Düzenleme sayfası eklendi

---
+++ No 04 - IKU Meetup/src/Alfabe.Client/Pages/Edit.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Alfabe.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Alfabe.Client.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly Proxy _client;
+
+        [BindProperty]
+        public Team Team{ get; set; }
+
+        public EditModel()
+        {
+            _client = new Proxy();
+        }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            Team = await _client.FindAsync(id);
+
+            if (Team == null)
+            {
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _client.UpdateAsync(id, Team);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
