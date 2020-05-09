5c2438b0b92d9516e6668bb566c4888e5056edc5
buraksenyurt <burakselimsenyurt@gmail.com>
2020-05-09 21:15:28 +0300

    Albüm fotoğrafı ekleme kodları

--- No 03 - Simple Razor App/src/MusicShop/Pages/Albums/Create.cshtml.cs
+++ No 03 - Simple Razor App/src/MusicShop/Pages/Albums/Create.cshtml.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using MusicShop.Data;
 using MusicShop.Models;
+using System.IO;
 
 namespace MusicShop.Pages.Albums
 {
@@ -17,6 +19,8 @@ namespace MusicShop.Pages.Albums
         private readonly MusicShopContext _context;
         [BindProperty]
         public Album AlbumInstance { get; set; }
+        [BindProperty]
+        public IFormFile CoverPhotoFile { get; set; }
         public SelectList ArtistList { get; set; }
 
         public CreateModel(ILogger<CreateModel> logger, MusicShopContext context)
@@ -27,22 +31,29 @@ namespace MusicShop.Pages.Albums
 
         public void OnGet()
         {
-            var artistNamesQuery=from a in _context.Artist
-                            orderby a.Name
-                            select a;
+            var artistNamesQuery = from a in _context.Artist
+                                   orderby a.Name
+                                   select a;
 
-            ArtistList=new SelectList(artistNamesQuery,"ID", "Name");
+            ArtistList = new SelectList(artistNamesQuery, "ID", "Name");
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
             var newAlbum = new Album();
 
+            _logger.Log(LogLevel.Warning, $"Uploaded file length {CoverPhotoFile.Length.ToString()} bytes");
+
             if (await TryUpdateModelAsync<Album>(
                  newAlbum,
-                 "albuminstance",   
-                 a => a.Title,a => a.ArtistID,a=>a.ReleaseYear))
+                 "albuminstance",
+                 a => a.Title, a => a.ArtistID, a => a.ReleaseYear))
             {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await CoverPhotoFile.CopyToAsync(memoryStream);
+                    newAlbum.Cover = memoryStream.ToArray();
+                }
                 _context.Album.Add(newAlbum);
                 await _context.SaveChangesAsync();
             }
