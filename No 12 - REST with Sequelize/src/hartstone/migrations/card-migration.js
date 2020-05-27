edfdfc5eac18645f2825accab6fe43b0ef2e0d0f
buraksenyurt <burakselimsenyurt@gmail.com>
2020-05-27 03:31:02 +0300

    name alanını unutmuşum

--- No 12 - REST with Sequelize/src/hartstone/migrations/card-migration.js
+++ No 12 - REST with Sequelize/src/hartstone/migrations/card-migration.js
@@ -9,6 +9,10 @@ module.exports = {
                 primaryKey: true,
                 type: Sequelize.INTEGER,
             },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
             description: {
                 type: Sequelize.STRING,
                 allowNull: false,
