153db1459d533d9d9c9f80a4d442fdac967f5be5
buraksenyurt <burakselimsenyurt@gmail.com>
2020-06-03 01:19:04 +0300

    EF için Console Loglama seçeneği eklendi.

--- No 14 - Generic Repository on Web Api/src/NorthwindApi/Context/NorthwindContext.cs
+++ No 14 - Generic Repository on Web Api/src/NorthwindApi/Context/NorthwindContext.cs
@@ -1,5 +1,7 @@
 using NorthwindApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace NorthwindApi.Context
 {
@@ -12,9 +14,16 @@ namespace NorthwindApi.Context
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
+        // EF tarafından SQLite'a giden sorguları görmek için console'a loglama yapacağız
+        public static readonly ILoggerFactory loggerFactory
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });
         public NorthwindContext(DbContextOptions<NorthwindContext> options)
         : base(options)
         {
         }
+
+        // Context nesnesinin ilgili operasyonunu override edip
+        // console loglayıcısını kullanmasını söylüyoruz
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseLoggerFactory(loggerFactory);
     }
 }
