3d671c1d7d2ba3ae6d2c815669a307fb04b43400
buraksenyurt <burakselimsenyurt@gmail.com>
2020-07-06 15:36:07 +0300

    Company ekleme özellikleri eklendi.

--- No 06 - DT-Training/Chapter03/GameWorldWeb/Pages/companies.cshtml.cs
+++ No 06 - DT-Training/Chapter03/GameWorldWeb/Pages/companies.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using NorthwindLib;
@@ -8,6 +9,8 @@ namespace GameWorldWeb.Pages
     public class CompaniesModel : PageModel
     {
         public IEnumerable<string> Companies { get; set; }
+        [BindProperty]
+        public Company Company { get; set; }
         private Northwind _db;
         // Veritabanı nesnesini constructor üstünden enjekte ediyoruz
         public CompaniesModel(Northwind db)
@@ -21,6 +24,17 @@ namespace GameWorldWeb.Pages
             //     "ZeniMax Media Inc","Take-Two Interactive Software Inc","Nintendo","Electronic Arts","Sierra","Valve","Rockstar Games","Activision Blizzard","Ubisoft","Sega Games Co."
             // };
             Companies = _db.Companies.Select(c => c.Name);
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Companies.Add(Company);
+                _db.SaveChanges();
+                return RedirectToPage("/companies");
+            }
+            return Page();
         }
     }
 
