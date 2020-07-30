af71be646bd56e7339cff3588f9aadeb5dc30551
buraksenyurt <burakselimsenyurt@gmail.com>
2020-07-30 18:44:40 +0300

    body parser buradan kaldırıldı

--- No 23 - A Simple React App/src/gamer/src/app.ts
+++ No 23 - A Simple React App/src/gamer/src/app.ts
@@ -3,17 +3,16 @@
 */
 import express, { Express } from "express"
 import mongoose from "mongoose"
-//import cors from "cors"
+import cors from "cors"
 import router from "./routes"
-import bodyParser from "body-parser"
+
 
 const app: Express = express()
 
 const PORT: string | number = process.env.PORT || 5556
 
-//app.use(cors()) // Cross Origin tanımlamaları için
+app.use(cors()) // Cross Origin tanımlamaları için
 app.use(router) // Http taleplerinin yönlendirmek için
-app.use(bodyParser.urlencoded({extended:true}))
 
 const uri: string = "mongodb://localhost:27017" //MongoDb bağlantı adresi
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
