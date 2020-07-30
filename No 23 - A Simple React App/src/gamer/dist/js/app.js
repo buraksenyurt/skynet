6632a459c8ffacde843e14f6d7778a62b5dfda6f
buraksenyurt <burakselimsenyurt@gmail.com>
2020-07-30 18:42:50 +0300

    cors etkinleştirildi.

--- No 23 - A Simple React App/src/gamer/dist/js/app.js
+++ No 23 - A Simple React App/src/gamer/dist/js/app.js
@@ -8,14 +8,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 */
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
-//import cors from "cors"
+const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const app = express_1.default();
 const PORT = process.env.PORT || 5556;
-//app.use(cors()) // Cross Origin tanımlamaları için
+app.use(cors_1.default()); // Cross Origin tanımlamaları için
 app.use(routes_1.default); // Http taleplerinin yönlendirmek için
-app.use(body_parser_1.default.urlencoded({ extended: true }));
 const uri = "mongodb://localhost:27017"; //MongoDb bağlantı adresi
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose_1.default.set("useFindAndModify", false);
