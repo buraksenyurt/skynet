ba6541d35300a6ede11437763b4b10ef6d32b7ee
buraksenyurt <burakselimsenyurt@gmail.com>
2020-07-30 18:44:54 +0300

    bodyParser eklendi.

--- No 23 - A Simple React App/src/gamer/src/routes/index.ts
+++ No 23 - A Simple React App/src/gamer/src/routes/index.ts
@@ -5,13 +5,15 @@
 
 import { Router } from "express"
 import { getPlayers, addPlayer,  deletePlayer } from "../controllers/players"
+import * as bodyParser from "body-parser"
 
 const router: Router = Router()
+router.use(bodyParser.json()) // Eğer bu satırı kullanmazsam, Post işlemi sonrasında body içeriğini alamıyorum. Undefined olarak dönüyor.
 
 router.get("/players", getPlayers) // /players şeklinde gelen Get talepleri getPlayers metodunca karşılanır
 
-router.post("/players/add", addPlayer) // players/add şeklinde gelen Post talepleri de add Player metoduna yönlendirilir
+router.post("/players", addPlayer) // Post talepleri add Player metoduna yönlendirilir
 
-router.delete("/players/delete/:id", deletePlayer) // players/delete/12 gibi id bilgisi ile birlikte gelen Http Delete talepleri de deletePlayer metoduna yönlendirilir
+router.delete("/players/:id", deletePlayer) // players/{object_id} gibi id bilgisi ile birlikte gelen Http Delete talepleri de deletePlayer metoduna yönlendirilir
 
 export default router
