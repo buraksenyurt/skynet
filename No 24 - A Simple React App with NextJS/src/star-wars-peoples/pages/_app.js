7b9ba76a07af13fd5e5cc2f91efa338fe135e64f
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-01 14:59:56 +0300

    Bootstrap desteği eklendi.

--- No 24 - A Simple React App with NextJS/src/star-wars-peoples/pages/_app.js
+++ No 24 - A Simple React App with NextJS/src/star-wars-peoples/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; //Bootstrap kullanmak için ekledik
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
