d818ead11cb1bda331764fbbb96d8dcf529c2e12
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-03 16:14:14 +0300

    Swagger entegrasyonu eklendi

--- No 25 - Hello Gin-Gonic/src/book-worm-api/go.mod
+++ No 25 - Hello Gin-Gonic/src/book-worm-api/go.mod
@@ -3,14 +3,26 @@ module book-worm-api
 go 1.14
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-openapi/spec v0.19.9 // indirect
+	github.com/go-openapi/swag v0.19.9 // indirect
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mailru/easyjson v0.7.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/swaggo/gin-swagger v1.2.0
+	github.com/swaggo/swag v1.6.7
+	github.com/urfave/cli v1.22.4 // indirect
+	github.com/urfave/cli/v2 v2.2.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.0
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
 	golang.org/x/sys v0.0.0-20200802091954-4b90ce9b60b3 // indirect
+	golang.org/x/tools v0.0.0-20200731060945-b5fad4ed8dd6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- No 25 - Hello Gin-Gonic/src/book-worm-api/main.go
+++ No 25 - Hello Gin-Gonic/src/book-worm-api/main.go
@@ -1,13 +1,32 @@
 package main
 
 // gin-gonic modülünü ekledik
+// Ayrıca Swagger desteği için gin-swagger modülü de eklendi
 import (
 	"log"
 	"net/http"
 
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	_ "book-worm-api/docs" //swag init ile eklenen dokümanın bildirimi
+	// Bunu eklememin sebebi çalışma zamanında swagger UI'a gidince aldığım Failed to load spec. hatası
+
 	"github.com/gin-gonic/gin"
 )
 
+// @title BookWorm Swagger API
+// @version 1.0
+// @description Servis kullanım rehberidir
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Burak Selim Şenyurt
+// @contact.email selim@buraksenyurt.com
+// @contact.url https://www.buraksenyurt.com
+
+// @BasePath /api/v1
+
+// @host localhost:5003
 func main() {
 	router := gin.Default()      // gin nesnesini örnekledik
 	api := router.Group("/api")  //ve api'nin
@@ -15,34 +34,72 @@ func main() {
 	quotes := v1.Group("/quote") //quotes isimli bir route tanımladık
 
 	// Bu route için kök adrese HTTP Get talebi gelirse tüm alıntıları listeyecek operasyonu çalıştırıyoruz
-	quotes.GET("/", func(c *gin.Context) {
-		// mongodb ile iletişim kuran quotedata içerisindeki ilgili metodu çağırdık
-		var quoteList, err = GetQuotes()
-		// Her ihtimale karşı listeyi çeken metot bir hata döndürmüş mü bakalım
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"quotes": quoteList}) // HTTP 200 OK ile birlikte çekilen listeyi geri yolluyoruz.
-	})
+	quotes.GET("/", GetAll)
 
 	// Kök adrese bir Post talebi gelirse bu sefer yeni bir alıntının eklenmesini sağlıyoruz
-	quotes.POST("/", func(c *gin.Context) {
-		var q quote
-		// Gelen JSON içeriğinin quote yapısına eşlenip eşlenemediği kontrol ediliyor
-		if err := c.ShouldBindJSON(&q); err != nil {
-			log.Print(err)
-			c.JSON(http.StatusBadRequest, gin.H{"msg": err}) // Eğer JSON içeriğinde sıkıntı varsa hata mesajı ile birlikte geriye HTTP 400 Bad Request dönüyoruz
-			return
-		}
-		id, err := AddQuote(&q) // Add metodu ile eklemeyi gerçekleştiriyoruz
-		if err != nil {         // eğer hata varsa bunu da değerlendirip geriye uygun bir HTTP durum kodu ile döndürüyoruz
-			c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
-			return
-		}
-		q.ID = id
-		c.JSON(http.StatusOK, gin.H{"added": q}) // Her şey yolundaysa HTTP 200 Ok
-	})
+	quotes.POST("/", Create)
+
+	/*
+		Swagger API dokümantasyon desteği için ekledik.
+		metot başlarında yer alan yorum satırları da Swagger UI tarafında değerlenecek
+	*/
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	_ = router.Run(":5003") // sunucuyu 5003 numaralı porttan hizmete açtık
+}
+
+/*
+	@Summary operasyon için açıklama kısmıdır.
+	@Produce ve @Accept ile HTTP operasyonun hangi tür içerikle çalıştığını söylüyoruz ki örnekte json.
+	@Param kısmında Body içinden quote tipinden bir değişken beklediğimizi ve zorunlu olduğunu ifade ederiz
+	@Sucess ile operasyon başarılı ise HTTP 200 dönüldüğünü ifade ederiz
+	@Failure kısımlarında metottan hangi tür HTTP hatalarının dönebileceğini ifade ediyoruz
+	@Router kısmında operasyon adresini HTTP POST metodu ile tetiklendiğini ifade ediyoruz
+*/
+
+// Create godoc
+// @Summary Yeni bir kitap alıntısı ekler
+// @Produce json
+// @Accept json
+// @Param quote body quote true "Alıntı Bilgileri"
+// @Success 200
+// @Failure 400
+// @Failure 500
+// @Router /quote/ [post]
+func Create(c *gin.Context) {
+	var q quote
+	// Gelen JSON içeriğinin quote yapısına eşlenip eşlenemediği kontrol ediliyor
+	if err := c.ShouldBindJSON(&q); err != nil {
+		log.Print(err)
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err}) // Eğer JSON içeriğinde sıkıntı varsa hata mesajı ile birlikte geriye HTTP 400 Bad Request dönüyoruz
+		return
+	}
+	id, err := AddQuote(&q) // Add metodu ile eklemeyi gerçekleştiriyoruz
+	if err != nil {         // eğer hata varsa bunu da değerlendirip geriye uygun bir HTTP durum kodu ile döndürüyoruz
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+		return
+	}
+	q.ID = id
+	c.JSON(http.StatusOK, gin.H{"added": q}) // Her şey yolundaysa HTTP 200 Ok
+}
+
+/*
+	{array} ile bir quote listesinin döneceğini belirtiyoruz
+*/
+
+// GetAll godoc
+// @Summary Tüm kitap alıntılarını döndürür
+// @Produce json
+// @Success 200 {array} quote
+// @Failure 500
+// @Router /quote/ [get]
+func GetAll(c *gin.Context) {
+	// mongodb ile iletişim kuran quotedata içerisindeki ilgili metodu çağırdık
+	var quoteList, err = GetQuotes()
+	// Her ihtimale karşı listeyi çeken metot bir hata döndürmüş mü bakalım
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"quotes": quoteList}) // HTTP 200 OK ile birlikte çekilen listeyi geri yolluyoruz.
 }
