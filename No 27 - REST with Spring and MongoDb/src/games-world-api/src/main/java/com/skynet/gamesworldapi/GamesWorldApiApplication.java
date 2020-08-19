b4ee7e66496b9000fd728a948ed6e67de8cf0b0d
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-19 19:26:38 +0300

    Spring projesi eklendi

---
+++ No 27 - REST with Spring and MongoDb/src/games-world-api/src/main/java/com/skynet/gamesworldapi/GamesWorldApiApplication.java
@@ -0,0 +1,13 @@
+package com.skynet.gamesworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesWorldApiApplication.class, args);
+	}
+
+}
