9188f64fc60db4bbe281570c8ec69c3e53d4974d
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-20 20:10:55 +0300

    Yazar bilgisi eklenip yeni migration oluşturuldu

--- No 28 - Come On Django/src/worldsite/quoteworld/models.py
+++ No 28 - Come On Django/src/worldsite/quoteworld/models.py
@@ -7,8 +7,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    # maksimum 30 karakter uzunluğunda olabilir
-    title = models.CharField(max_length=30)
+    # maksimum 30 karakter uzunluğunda olabilir ve null olamaz
+    title = models.CharField(max_length=30, null=False)
 
     # shell'deki sorgu çıktıların güzel görünsün diye __str__ fonksiyonunu eziyoruz
     def __str__(self):
@@ -19,10 +19,11 @@ class Quote(models.Model):
     # Quote'lar Category'ye bağlıdır. one-to-many relations söz konusu
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     # maksimum 500 karakter olabilir
-    content = models.CharField(max_length=500)
+    content = models.CharField(max_length=500, null=False)
     # bu da varsayılan değeri 1 olan integer bir alan
     popularity_point = models.IntegerField(default=1)
+    author = models.CharField(max_length=50, null=False)
 
     # benzer şekilde burada da ezdik
     def __str__(self):
-        return self.content+' '+self.popularity_point
+        return self.content
