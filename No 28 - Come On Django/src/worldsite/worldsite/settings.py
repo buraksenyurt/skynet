41296b651cdb49c7eb962eadc6a994271d0dcdbe
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-20 19:36:53 +0300

    Migration işlemleri

--- No 28 - Come On Django/src/worldsite/worldsite/settings.py
+++ No 28 - Come On Django/src/worldsite/worldsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quoteworld.apps.QuoteworldConfig', # Migration işleminin QuoteWorld uygulamasını da ele alması için eklendi
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'worldsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'bigkahunaclub.sqlite3',
     }
 }
 
