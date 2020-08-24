1f437c8d28c4a6209eb283bc1337bcbd9a67eab4
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-24 10:51:43 +0300

    Proje açılışı yapıldı

---
+++ No 30 - Batch Job with Spring/src/products-importer/src/main/java/com/azon/portal/productsimporter/ProductsImporterApplication.java
@@ -0,0 +1,13 @@
+package com.azon.portal.productsimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsImporterApplication.class, args);
+	}
+
+}
---
+++ No 30 - Batch Job with Spring/src/products-importer/src/test/java/com/azon/portal/productsimporter/ProductsImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azon.portal.productsimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
