3b3080cee041b2a88288c2e14d934beac9ebf842
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-26 16:55:43 +0300

    Proje açılışı yapıldı

---
+++ No 31 - Web App with Spring and PostgreSQL/src/quote-world-web/src/main/java/com/learning/quoteworldweb/QuoteWorldWebApplication.java
@@ -0,0 +1,13 @@
+package com.learning.quoteworldweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteWorldWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteWorldWebApplication.class, args);
+	}
+
+}
