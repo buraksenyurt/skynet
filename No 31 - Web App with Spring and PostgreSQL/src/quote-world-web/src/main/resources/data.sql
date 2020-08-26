241ef2aa6981a9b5bd8fb9349ea7b7132874f199
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-26 20:44:33 +0300

    dosya adları değişti

---
+++ No 31 - Web App with Spring and PostgreSQL/src/quote-world-web/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO categories(title,quoteCount) VALUES ('Türk Edebiyatından',150);
+INSERT INTO categories(title,quoteCount) VALUES ('Futuristlerde',58);
+INSERT INTO categories(title,quoteCount) VALUES ('İlham Veren',18);
---
+++ No 31 - Web App with Spring and PostgreSQL/src/quote-world-web/src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories(id serial PRIMARY KEY, title varchar(50),quotecount integer);
