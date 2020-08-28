c13d2980c582b07d1f0b5d2e142bcfc2c83a5636
buraksenyurt <burakselimsenyurt@gmail.com>
2020-08-28 11:31:31 +0300

    Merhaba Rust

---
+++ No 32 - Who are you Rust/src/WhoAreYouRust.rs
@@ -0,0 +1,9 @@
+fn // fonksiyon bildirimi anlamına gelir
+main(){ // Tahmin edileceği üzere programın giriş noktası. Önceden tanımlı fonksiyondur
+    println!("I hate hello world!"); // Sondaki ünlem işareti println'in bir macro olduğu ifade eder.
+
+    /*
+        Makrolar fonksiyonların genişletilmiş hali olarak ifade ediliyor. Çalışma zamanındaki meta bilgileri ile konuşma olanakları varmış.
+        Sanırım bunu ilerde daha net anlarım.
+    */
+}
