664f69f5f8d897a14f72407a8ef71f69bc923157
buraksenyurt <burakselimsenyurt@gmail.com>
2020-09-05 22:29:13 +0300

    String öğrenilmeye başlandı

--- No 32 - Who are you Rust/src/collections/src/main.rs
+++ No 32 - Who are you Rust/src/collections/src/main.rs
@@ -78,6 +78,103 @@ fn main() {
         Product::Title(String::from("12li Su Bardağı")),
         Product::ListPrice(12.90),
     ];
+
+    /*
+        Gelelim Rust standart kütüphanesi ile birlikte gelen diğer bir koleksiyon olan String'e.
+        String'i aslında byte'lar koleksiyonu olarak düşünmek daha doğru olabilir.
+
+        String'in birkaç oluşturulma şekli var. Örneğin new ile tanımlanıp literal bir string üstünden to_string çağrısı ile
+        ya da from fonksiyonu ile üretilebililir.
+
+        String veri türü UTF-8 formatında kodlanmış içerikleri kullanabilir. Bu sebepten whatsup değişkeninde olduğu gibi pek çok dili destekler.
+
+        String'leri birleştirmek veya bir String'e başka bir String parçası eklemek için push_str ve
+        tek bir karakter eklemek için push fonksiyonlarını kullanabiliriz.
+
+        Tabii + operatörü de String'leri birleştirmek için kullanılabilir.
+        Çok fazla birleştirilecek String varsa + operatörü (ki add fonksiyonuna karşılık gelir) yerine,
+        format! isimli macro'yu kullanmak daha uygundur.
+
+    */
+    let mut aloha = String::new(); // aşağıda değerini değiştireceğimiz için mutable yaptık
+    let incoming_data = "Alofortanfane";
+    aloha = incoming_data.to_string();
+
+    println!("{}", aloha);
+
+    let raining_day = String::from("Một ngày mưa.");
+    println!("{}", raining_day);
+
+    let mut quote = String::from("Siz ne kadar veri üretirseniz");
+    quote.push_str(", organize suç örgütleri de o kadar tüketir");
+    quote.push('!');
+    println!("{}", quote);
+    quote.push_str(" Marc Goodman - Geleceğin Suçları");
+    println!("{}", quote);
+
+    /*
+        + operatörünü kullandığımızda & ile referans adreslerine ulaşmamız gerekir.
+        Bunun sebebi aslında + operatörünün işaret ettiği add metodunun (fn add(self, s: &str) -> String şeklinde yazılmıştır)
+        &str şeklinde referans istemesidir.
+    */
+    let s1 = "Ne".to_string();
+    let s2 = String::from("güzel");
+    let s3 = String::from("bir");
+    let s4 = String::from("gün!");
+    let last_word = s1 + " " + &s2 + " " + &s3 + " " + &s4; //s1'e sırasıyla s2, s3 ve s4 değişkenlerinin referans adresleri eklendi
+    println!("{}", last_word);
+
+    let black = String::from("black");
+    let white = String::from("white");
+    let black_and_white = format!("{} {} {}", black, "or", white);
+    println!("{}", black_and_white);
+
+    /*
+        String veri türünde uzunluk aslında kullanılan karakterlerin byte olarak kapladıkları yere göre değişir.
+        Eğer Unicode karakter varsa bu UTF-8 kodlaması sonucu 2 byte olarak ele alınır ve uzunluk değişir.
+        Belki de bu sebepten ötürü String türünde indis operatörü kullanılamaz.
+    */
+    let siyah = "đen";
+    println!(
+        "Siyah vietnamca `{}` olarak yazılır. Rust için uzunluğu {}. Halbu ki sen 3 karakter saydın :)",
+        siyah,
+        siyah.len()
+    );
+    // let second = siyah[1]; // the type `str` cannot be indexed by `{integer}` hatası döner
+
+    /*
+        Bir String içinden belli bir dilimi almak (slice) mümkündür ancak dikkat etmek gerekir.
+        Çünkü denk gelen byte bir karakter olarak ifade edilemeyebilir.
+        Aşağıdaki kod parçası derlenecektir ama çalışma zamanında bir panic oluşacaktır.
+        thread 'main' panicked at 'byte index 1 is not a char boundary; it is inside 'đ' (bytes 0..2) of `đen`'
+    */
+    // let a_bit_off_word = &siyah[0..1];
+    // println!("{}", a_bit_off_word);
+
+    /*
+        String içerisindeki karakterleri veya byte'ları dolaşmanın en güzel yolu chars ve bytes fonksiyonlarından yararlanmaktır
+    */
+    println!();
+
+    let rusca_bir_seyler = String::from("Добрый день умереть.");
+
+    for c in rusca_bir_seyler.chars() {
+        print!("{} ", c);
+    }
+    println!();
+    for b in rusca_bir_seyler.bytes() {
+        print!("{} ", b);
+    }
+    println!();
+
+    /*
+        String ile başka neler yapabiliriz bakalım.
+        Mesela String içindeki karakterleri bir vector'e indirebiliriz.
+    */
+    let char_vector: Vec<char> = rusca_bir_seyler.chars().collect();
+    for c in char_vector {
+        println!("`{}` ", c);
+    }
 }
 
 enum Product {
