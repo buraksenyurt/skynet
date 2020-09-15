ddbd5213db65b8a033e2e9f34f166ff506baf55c
buraksenyurt <burakselimsenyurt@gmail.com>
2020-09-15 20:54:16 +0300

    birkaç milyon oyun bilgisi eklendi :P

--- No 32 - Who are you Rust/src/iterators2/src/main.rs
+++ No 32 - Who are you Rust/src/iterators2/src/main.rs
@@ -9,8 +9,18 @@
 struct Game {
     name: String,
     year: u16,
-    company: String,
+    publisher: String,
     value: f32,
+    platform: Platform,
+}
+
+/*
+    Oyunun hangi console'da oynandığı bilgisini de bir enum ile tutalım
+*/
+enum Platform {
+    Commodore64,
+    Atari2600,
+    Atari5200,
 }
 
 /*
@@ -26,18 +36,71 @@ fn get_by_year(games: Vec<Game>, _year: u16) -> Vec<Game> {
     Örnek birkaç oyun bilgisi yüklediğimiz fonksiyon
 */
 fn load_samples() -> Vec<Game> {
-    vec![Game {
-        name: String::from("Crazy Cars II"),
-        year: 1988,
-        company: String::from("Titus"),
-        value: 1.5,
-    },
-    Game{
-        name: String::from("Crazy Cars II"),
-        year: 1988,
-        company: String::from("Titus"),
-        value: 1.5, 
-    }]
+    vec![
+        Game {
+            name: String::from("Crazy Cars II"),
+            year: 1988,
+            publisher: String::from("Titus"),
+            value: 1.5,
+            platform: Platform::Commodore64,
+        },
+        Game {
+            name: String::from("1942"),
+            year: 1986,
+            publisher: String::from("Elit"),
+            value: 2.85,
+            platform: Platform::Commodore64,
+        },
+        Game {
+            name: String::from("Pitstop II"),
+            year: 1984,
+            publisher: String::from("Epyx"),
+            value: 0.55,
+            platform: Platform::Commodore64,
+        },
+        Game {
+            name: String::from("The Last Ninja"),
+            year: 1987,
+            publisher: String::from("System 3"),
+            value: 1.49,
+            platform: Platform::Commodore64,
+        },
+        Game {
+            name: String::from("Spy Hunter"),
+            year: 1983,
+            publisher: String::from("US Gold"),
+            value: 2.40,
+            platform: Platform::Commodore64,
+        },
+        Game {
+            name: String::from("3-D Tic Tac Toe"),
+            year: 1980,
+            publisher: String::from("Atari"),
+            value: 6.75,
+            platform: Platform::Atari2600,
+        },
+        Game {
+            name: String::from("Asteroids"),
+            year: 1981,
+            publisher: String::from("Atari"),
+            value: 6.70,
+            platform: Platform::Atari2600,
+        },
+        Game {
+            name: String::from("Gremlins"),
+            year: 1986,
+            publisher: String::from("Atari"),
+            value: 2.75,
+            platform: Platform::Atari5200,
+        },
+        Game {
+            name: String::from("Mario Bros."),
+            year: 1988,
+            publisher: String::from("Nintendo"),
+            value: 9.85,
+            platform: Platform::Atari5200,
+        },
+    ]
 }
 
 fn main() {}
