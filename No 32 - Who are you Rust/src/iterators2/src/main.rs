72a3b754dbe055cbd39a7889772e1d1427d646f0
buraksenyurt <burakselimsenyurt@gmail.com>
2020-09-15 20:43:48 +0300

    bir arama fonksiyonu ve game yapısı eklendi

--- No 32 - Who are you Rust/src/iterators2/src/main.rs
+++ No 32 - Who are you Rust/src/iterators2/src/main.rs
@@ -1,8 +1,43 @@
 /*
     bir iterator kullanım örneği daha.
+    Bu kez kendi veri yapımızın alanları üzerinde filtreleme işlemi gerçekleştiriyoruz.
 */
 
-
-
-fn main() {
+/*
+    Birkaç klasik oyun bilgisini tutacak bir struct
+*/
+struct Game {
+    name: String,
+    year: u16,
+    company: String,
+    value: f32,
 }
+
+/*
+    Yıla göre oyunları döndüren bir fonksiyon.
+    Game türünden vector parametre olarak gelir, _year değerine göre filtreleme yapılır
+    ve bu kritere uyan oyunlar geriye dönülür
+*/
+fn get_by_year(games: Vec<Game>, _year: u16) -> Vec<Game> {
+    games.into_iter().filter(|g| g.year == _year).collect()
+}
+
+/*
+    Örnek birkaç oyun bilgisi yüklediğimiz fonksiyon
+*/
+fn load_samples() -> Vec<Game> {
+    vec![Game {
+        name: String::from("Crazy Cars II"),
+        year: 1988,
+        company: String::from("Titus"),
+        value: 1.5,
+    },
+    Game{
+        name: String::from("Crazy Cars II"),
+        year: 1988,
+        company: String::from("Titus"),
+        value: 1.5, 
+    }]
+}
+
+fn main() {}
