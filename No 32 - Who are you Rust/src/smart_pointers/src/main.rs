55eb1c424ffe048a916b6957479f6989e0775e40
buraksenyurt <burakselimsenyurt@gmail.com>
2020-09-18 22:41:20 +0300

    cons list'in hata veren versiyonu tanımlandı

--- No 32 - Who are you Rust/src/smart_pointers/src/main.rs
+++ No 32 - Who are you Rust/src/smart_pointers/src/main.rs
@@ -1,7 +1,7 @@
 /*
     Pointer denilince aklımıza bellekteki bir bölgenin adresini tutan işaretçi gelir.
-    Rust dilinde pointer'ların en bilinen tipi referans türleridir. 
-    Ancak birde Smart Pointer adı verilen veri yapısı vardır. Smart Pointer'da verinin adresini taşır ama 
+    Rust dilinde pointer'ların en bilinen tipi referans türleridir.
+    Ancak birde Smart Pointer adı verilen veri yapısı vardır. Smart Pointer'da verinin adresini taşır ama
     ekstradan metadata bilgisi de barındırır.
     Hatta şu ana kadar birkaç smart pointer kullanmışızdır. String ve Vec veri yapıları Smart Pointer türündendir.
     Sadece rerenas adresi taşımazlar bununla birlikte ek bilgi de barındırırlar. Vector'ün başlangıç kapasitesi gibi.
@@ -11,6 +11,7 @@
 
     Bu örnekte smart pointer türlerini incelemeye çalışacaım.
 */
+
 fn main() {
     /*
         En bilinen Smart Pointer türlerinden birisi Box<T>
@@ -21,4 +22,28 @@ fn main() {
         gibi durumlarda tercih edilir.
     */
 
+    let a_number = Box::new(3.1415); // normalda stack'te duracak bir f32 verisini Heap'te kutuladık
+    println!("{}", a_number);
+
+    /*
+        Rust, derleme zamanında tiplerin ne kadar yer tutacağını bilmek ister.
+        Fonksiyonel dillerde rastlanan cons list gibi türler ise recursive özellik gösterirler ve tipin ne kadar yer tutacağı kestirilemez.
+        cons Lisp kökenlidir ve iki argüman alan bir fonksiyondur. const list, cons fonksiyonunu recursive olarak çağıran bir elemanlar dizisidir.
+        Son elemanda Nil görene kadar bu liste devam edebilir.
+
+
+    */
 }
+
+/*
+        ConsList enum türüne bakalım. Cons fonksiyonunu ve Nil değerini içeriyor.
+        Cons fonksiyonu da i32 türünden bir değer ve yine bir ConsList enum türü alıyor. İşte recursive veri yapısı.
+        Bunu bu haliyle bırakırsak derleme zamanı 'recursive type `ConsList` has infinite size' şeklinde hata döner.
+        O yüzden ConsListV2 şeklinde tanımlayıp kullanmamız gerekiyor.
+*/
+enum ConsList {
+    Cons(i32, ConsList),
+    Nil,
+}
+
+//use crate::ConsList::{Cons, Nil};
