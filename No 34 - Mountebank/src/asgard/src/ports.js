9be8dcb841c448bb89adcf05f8d5d017c0316108
buraksenyurt <burakselimsenyurt@gmail.com>
2020-09-22 18:08:53 +0300

    Süslü parantezi unutmuşum be yaa

--- No 34 - Mountebank/src/asgard/src/ports.js
+++ No 34 - Mountebank/src/asgard/src/ports.js
@@ -1,4 +1,5 @@
 module.exports = {
-    port: 5500, // Mountebank uygulamasının ana servis adresidir
-    ping_service_port: 5501, // ping-pong servisinin kullanacağı adrestir
-    city_service_port: 5502 // bu çalışacağımız şehir bilgilerini getiren servise ait bir adres
+    server: 5500, // Mountebank uygulamasının ana servis adresidir
+    ping_service: 5501, // ping-pong servisinin kullanacağı adrestir
+    city_service: 5502, // bu çalışacağımız şehir bilgilerini getiren servise ait bir adres
+};
