3d71a8e2f4b829d2b7f17429b1851790e7ec3631
buraksenyurt <burakselimsenyurt@gmail.com>
2020-09-22 22:18:40 +0300

    mock servisin uygulama ayağa kalkınca register edilmesi

--- No 34 - Mountebank/src/asgard/src/index.js
+++ No 34 - Mountebank/src/asgard/src/index.js
@@ -2,6 +2,13 @@
 const mb = require('mountebank');
 const ports = require('./ports');
 
+/* 
+    Mountebank uygulamasını ayağa kaldırdığımızda, yazdığımız mock servislerin de 
+    etkinleştirilmesini sağlayabiliriz.
+    then fonksiyonuna odaklanın.
+*/
+const pingService = require('./ping-service');
+
 // Yeni bir mountebank örneği oluşturuyoruz
 const server_instance = mb.create({
     port: ports.server,
@@ -9,4 +16,6 @@ const server_instance = mb.create({
     logfile: '../mb.log',
     protofile: '../protofile.json',
     ipWhitelist: ['*']
+}).then(function () {
+    pingService.register();
 });
