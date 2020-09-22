a2dfcd4820f738f77d3ecfffc305bff651f517fd
buraksenyurt <burakselimsenyurt@gmail.com>
2020-09-22 17:56:07 +0300

    Mock servis adresleri için konfigurasyon dosyası eklendi

---
+++ No 34 - Mountebank/src/asgard/src/ports.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: 5500, // Mountebank uygulamasının ana servis adresidir
+    ping_service_port: 5501, // ping-pong servisinin kullanacağı adrestir
+    city_service_port: 5502 // bu çalışacağımız şehir bilgilerini getiren servise ait bir adres
