a2c8071c780bc1a9806f89bfab8644b6f6bf3a5a
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-01 00:23:01 +0300

    SqlKata yapacak web api projesini ekledim

---
+++ No 35 - Hiyaaa This is SqlKata/src/NorthwindApi/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NorthwindApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation($"{DateTime.UtcNow.ToLongTimeString()} - ProductController - GET");
+            return "Pong";
+        }
+    }
+}
