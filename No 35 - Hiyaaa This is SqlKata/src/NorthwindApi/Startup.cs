4e80e1b937059685f72ce40056e1d010ad451f70
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-01 00:48:26 +0300

    QueryFactory nesnesini servise kaydettim

--- No 35 - Hiyaaa This is SqlKata/src/NorthwindApi/Startup.cs
+++ No 35 - Hiyaaa This is SqlKata/src/NorthwindApi/Startup.cs
@@ -10,6 +10,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+/*
+    Postgresql ve SqlKata için gerekli namespace bildirimleri
+*/
+using SqlKata;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+using Npgsql; 
 
 namespace NorthwindApi
 {
@@ -26,7 +33,22 @@ namespace NorthwindApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            /*
+                QueryFactory sınıfını burada kayıt edip controller'lara constructor üzerinde enjekte ederek kullandırtabiliriz.
+                Oluştururken Postgresql bağlantı bilgisini veriyoruz.
+                Ayrıca sorgular için gerekli derleyici nesnesi de üretiliyor
+            */
+            services.AddScoped(factory =>
+            {
+                return new QueryFactory
+                {
+                    Compiler = new PostgresCompiler(),
+                    Connection = new NpgsqlConnection("Host=localhost;Username=username;Password=pass;Database=db name")
+                };
+            });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
