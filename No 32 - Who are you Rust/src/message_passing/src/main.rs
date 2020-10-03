aa16798e11c5264ff9d543afae2b61ca76b76924
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-03 17:21:33 +0300

    ownershiplik önemli

--- No 32 - Who are you Rust/src/message_passing/src/main.rs
+++ No 32 - Who are you Rust/src/message_passing/src/main.rs
@@ -35,22 +35,41 @@ fn main() {
         println!("#1 Jennifer tepeden nehre bir plastik ördek bırakıyor. Aklında bir sayı var.");
         let calculated_value = 3.1415;
         tx.send(calculated_value).unwrap(); // transmitter ile kanala mesajımızı/değeri bırakıyoruz
-        
-        /*
-            worker'lar pek tabi eş zamanlı olarak işe başlarlar.
-            Aşağıda worker1 sembolik olarak uzun süre bir iş yapsa da,
-            yukarıda kanala bir mesaj bırakmıştır ve diğer thread'ler 
-            bu mesajı duraksatmaya aldırmadan alıp kullanabilirler ;)
-        */
-        thread::sleep(Duration::from_secs(3)); 
+                                            /*
+                                                worker'lar pek tabi eş zamanlı olarak işe başlarlar.
+                                                Aşağıda worker1 sembolik olarak uzun süre bir iş yapsa da,
+                                                yukarıda kanala bir mesaj bırakmıştır ve diğer thread'ler
+                                                bu mesajı duraksatmaya aldırmadan alıp kullanabilirler ;)
+                                            */
+        thread::sleep(Duration::from_secs(3));
     });
 
     let worker2 = thread::spawn(move || {
         println!("#2 Alice, Jennifer'ın gönderdiği plastik ördeği bekliyor.");
         let received_value = rx.recv().unwrap(); //receiver ile kanala bırakılan mesajı yakalıyoruz
-        println!("Kanala bırakılan plastik ördeğin aklındaki sayı: {}",received_value);
+        println!(
+            "Kanala bırakılan plastik ördeğin aklındaki sayı: {}",
+            received_value
+        );
     });
 
     worker1.join().unwrap();
     worker2.join().unwrap();
+
+    /*
+        #2 Aşağıdaki kod bloğu safe concurency sebebiyle derlenmez.
+        Transmitter değişkeninin send metodu, outgoing referansının sahipliğini alır 
+        ve bu değişken gönderildikten sonra bu sahiplik receiver'a geçer. 
+        Bu nedenle spawn bloğunda send çağrısı sonrası outgoing değişkeni artık kullanılamaz.
+        Bunun sebebi bir thread'in kanala bıraktığı değeri sonradan kendisinin değiştirmesini engellemektir.
+        Lakin gönderilen değişkenin gönderildiği haliyle receiver tarafından kullanılmasını isteriz.
+    */
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let outgoing = String::from("Eco Eco Bravo 6 Eco...Burası Kartal Kondu. Tamam");
+        tx.send(outgoing).unwrap();
+        // println!("{}", outgoing); // Derleme hatasını görmek için bu satırı etkinleştirin
+    });
+    let incoming = rx.recv().unwrap();
+    println!("{}", incoming);
 }
