91b0757dbeb313cdc3c151477b9aa66b25381ed7
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-04 18:10:40 +0300

    Değişik bir mutex kodu oldu yahu

--- No 32 - Who are you Rust/src/mutexes/src/main.rs
+++ No 32 - Who are you Rust/src/mutexes/src/main.rs
@@ -11,7 +11,7 @@
 
     Bir thread Mutex'e alınan bir veriyi kullanmak istediğinde bunu öncelikle ona sorar ve eğer müsaitse verinin kilidini(lock) alır. İşi bitince de
 */
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
@@ -21,27 +21,62 @@ fn main() {
         İki thread Mutex kullanırken.
         worker_sue içinde "use of moved value" derleme zamanı hatası alırız.
         *collector satırında ise "borrow of moved value" derleme zamanı hatasını alırız.
-        
-    */
-    // Mutex nesnesini oluşturuyoruz. Tutacağı veri 32 bit integer ve değeri 1
-    let collector = Mutex::new(1);
 
-    // Bir thread başlatılıyor
+    */
+    // // Mutex nesnesini oluşturuyoruz. Tutacağı veri 32 bit integer ve değeri 1
+    // let collector = Mutex::new(1);
+
+    // // Bir thread başlatılıyor
+    // let worker_joe = thread::spawn(move || {
+    //     // mutex'in kilidini alıyoruz ve işimiz bitene kadar thread'i blokluyoruz
+    //     let mut point = collector.lock().unwrap();
+    //     // Burada kilidi bizde olan Mutex verisinin değerini değiştirdik
+    //     *point += 5;
+    // });
+    // // Scope dışına çıktığımız anda Mutex'in kilidini de devretmiş olduk. Dolayısıyka başka bir thread artık bu kilidi alabilir.
+
+    // /*
+    //     Aşağıdaki satırda "use of moved value" hatası alınır
+    //     Rust, collector'un mülkiyetinin birden fazla thread'e alınamayacağını söyler.
+    //     Dikkat edin. Üst taraftaki thread için derleyici kızmaz. Aşağıdaki move || satırının altını çizerek kızar.
+    // */
+    // let worker_sue = thread::spawn(move || {
+    //     let mut point = collector.lock().unwrap();
+    //     *point += 3;
+    // });
+
+    // worker_joe.join().unwrap();
+    // worker_sue.join().unwrap();
+
+    // println!("{}", *collector.lock().unwrap());
+
+    /*
+        #2
+
+        Üstteki kodda bildiğiniz üzere mülkiyet sorunu yaşadık.
+        Thread Safe olarak Mutex'i diğer thread'lerin de kullanılmasını sağlamak için,
+        Atomic Reference Counting Arc<T> tipinden yararlanabiliriz.
+
+        Kodun çalışır hali aşağıdaki gibidir.
+
+        Mutex verisini değiştirmek ve son halini almak için Arc üstünde klonladığımız referanslar olduğuna dikkat edelim.
+    */
+    let main_collector = Arc::new(Mutex::new(1));
+    let mid_collector = Arc::clone(&main_collector);
+    let last_collector = Arc::clone(&main_collector); // Buna neden ihtiyaç duydum?
+
     let worker_joe = thread::spawn(move || {
-        // mutex'in kilidini alıyoruz ve işimiz bitene kadar thread'i blokluyoruz
-        let mut point = collector.lock().unwrap();
-        // Burada kilidi bizde olan Mutex verisinin değerini değiştirdik
+        let mut point = main_collector.lock().unwrap();
         *point += 5;
     });
-    // Scope dışına çıktığımız anda Mutex'in kilidini de devretmiş olduk. Dolayısıyka başka bir thread artık bu kilidi alabilir.
 
     let worker_sue = thread::spawn(move || {
-        let mut point = collector.lock().unwrap();
+        let mut point = mid_collector.lock().unwrap();
         *point += 3;
     });
 
     worker_joe.join().unwrap();
     worker_sue.join().unwrap();
 
-    println!("{}", *collector.lock().unwrap());
+    println!("{}", *last_collector.lock().unwrap());
 }
