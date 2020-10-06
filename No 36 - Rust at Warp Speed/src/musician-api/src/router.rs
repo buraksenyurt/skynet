0724e36dfa777c262e02a5024a2473370b5f06a3
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-06 10:25:25 +0300

    Id bilgisine göre yönlendirme yapan fonksiyon eklendi

--- No 36 - Rust at Warp Speed/src/musician-api/src/router.rs
+++ No 36 - Rust at Warp Speed/src/musician-api/src/router.rs
@@ -1,4 +1,4 @@
-/*  
+/*
     Burası router işlemlerini yönettiğimiz yer
     Mesela doğrudan /products adresine gelecek HTTP Get taleplerine karşılık get_all'un çalışmasını sağlıyoruz.
     Yönlendirme adresleri için warp'un path fonksiyonundan yararlanıyoruz.
@@ -14,22 +14,36 @@ use crate::rust_lite::ProductDb;
 pub fn setup(
     db: ProductDb,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    get_all(db.clone())
+    /*
+        Sırasıyla HTTP taleplerini ele alacak fonksiyonlar bildiriliyor.
+        (get_all, get_by_id...)
+        Bu fonksiyonlar eş zamanlı gelecek istemci taleplerini işlerken db nesnesinin
+        (ProductDb) thread-safe klonlanmış bir versiyonlarını parametre olarak alıyorlar.
+    */
+    get_by_id(db.clone()).or(get_all(db))
+    //get_all(db.clone()).or(get_by_id(db)) // Bomba soru için eklendi
 }
 
+// Burası /products için HTTP Get talebi geldiğinde çalışacak olan fonksiyon
 fn get_all(
     db: ProductDb,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path("products")
         .and(warp::get())
-        .and(with_db(db))
+        .and(warp::any().map(move || db.clone())) //Veritabanı referansını (ki bu örnekte Product_Db nesnesi) router tarafına referans olarak paslayabilmek için kullanılan yardımcı fonksiyon.
         .and_then(product_repository::get_products)
 }
 
 /*
-    Veritabanı referansını (ki bu örnekte Product_Db nesnesi) router tarafına referans olarak 
-    paslayabilmek için kullanılan yardımcı fonksiyon.
+    URL'den gelen id değerine göre ürün bilgisi getirecek fonksiyonumuz.
+    products/{id} şeklinde bir map söz konusudur.
 */
-fn with_db(db: ProductDb) -> impl Filter<Extract = (ProductDb,), Error = Infallible> + Clone {
-    warp::any().map(move || db.clone())
+fn get_by_id(
+    db: ProductDb,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    // path! makrosu URL tarafında parametre kullanımını kolaylaştırır
+    warp::path!("products" / String)
+        .and(warp::get())
+        .and(warp::any().map(move || db.clone())) //with_db fonksiyonelliğini bu şekilde closure olarak da kullanabiliriz
+        .and_then(product_repository::get_by_id)
 }
