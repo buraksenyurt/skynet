fa685feb99a222e674978cfbb257bb522df3f423
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-06 10:25:00 +0300

    Id bilgisine göre ürün döndüren fonksiyon eklendi

--- No 36 - Rust at Warp Speed/src/musician-api/src/product_repository.rs
+++ No 36 - Rust at Warp Speed/src/musician-api/src/product_repository.rs
@@ -2,13 +2,42 @@ use crate::models::Product;
 use crate::rust_lite::ProductDb;
 use std::convert::Infallible;
 
+use warp::{self, http::StatusCode};
+
 /*
     Ürün listesini Thread-safe döndüren fonksiyon
     rust_lite paketindeki Product_Db'yi kullanıyor ki O da products.json dosyası ile beslenmekte
 */
 pub async fn get_products(db: ProductDb) -> Result<impl warp::Reply, Infallible> {
+
+    println!("get_products fonksiyonu çağrıldı");
+
     let products = db.lock().await; // Arc klonlandı ve thread-safety sağlandı
     let products: Vec<Product> = products.clone(); // Mutex içindeki veriyi de klonladık
                                                    // json formatlı olarak geriye döndürdük
     Ok(warp::reply::json(&products))
+}
+
+/*
+    Id bilgisine göre ürün bilgisi yine Thread-Safe döndüren fonksiyon.
+    Bu fonksiyonu servise gelen talepleri karşılayan router kullanıyor.
+*/
+pub async fn get_by_id(id: String, db: ProductDb) -> Result<Box<dyn warp::Reply>, Infallible> {
+
+    println!("get_by_id fonksiyonu çağrıldı");
+
+    // Önce db nesnesini tutan Mutex thread-safe klonlanır
+    let products = db.lock().await;
+    // Amelece olacak ama tüm vector nesnelerini bir iterasyon ile dolaşıyoruz
+    for p in products.iter() {
+        // parametre olarak gelen id'yi bulursak
+        if p.id == id {
+            // Bulunan vector satırının json formatına dönüştürülmüş halinin 
+            // Heap'e çekilmiş bir versiyonunu döndürüyoruz
+            return Ok(Box::new(warp::reply::json(&p)));
+        }
+    }
+
+    // Eğer kayıt bulunamazsa HTTO 404 Not Found durumunu döndüreceğiz
+    Ok(Box::new(StatusCode::NOT_FOUND)) 
 }
--- No 36 - Rust at Warp Speed/src/musician-api/src/rust_lite.rs
+++ No 36 - Rust at Warp Speed/src/musician-api/src/rust_lite.rs
@@ -1,5 +1,5 @@
 /*
-    products.json içerisindeki veriyi belleğe çekip bir vector'e parse eden ve 
+    products.json içerisindeki veriyi belleğe çekip bir vector'e parse eden ve
     asenkron olarak farklı thread'lerin güvenli bir şekilde kullanabilmesine
     imkan sağlayan fonkisyonelliği tutan dosyamız.
 */
@@ -21,16 +21,21 @@ pub type ProductDb = Arc<Mutex<Vec<Product>>>;
     veriyi JSON'dan ters serileştirip Product türünden vector nesnesine
     aktarıyoruz ve bunu kullanan Mutex'imizi örnekleyip geriye döndürüyoruz.
 
-    Veriyi asenkron olarak gelecek Web API isteklerinin eş zamanlı kullanabilmesi 
+    Veriyi asenkron olarak gelecek Web API isteklerinin eş zamanlı kullanabilmesi
     için Mutex<T> türünden yararlandık. Thread Safety olmasını da Arc tipinden faydalanarak sağladık.
 */
 pub fn load() -> ProductDb {
+    println!("Veritabanı yükleme adımı");
+
     let file = File::open("./products.json");
     match file {
         Ok(json) => {
             let data = from_reader(json).unwrap();
             Arc::new(Mutex::new(data))
         }
-        Err(_) => Arc::new(Mutex::new(Vec::new())),
+        Err(e) => {
+            println!("{}", e); // Olası hata durumuna karşı
+            Arc::new(Mutex::new(Vec::new()))
+        }
     }
 }
