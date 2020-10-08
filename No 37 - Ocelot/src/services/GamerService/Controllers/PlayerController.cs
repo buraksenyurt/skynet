ccc16b77416e10870e1c136199b00597e74378f1
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-08 16:22:35 +0300

    kobay GamerService'i ekledim

---
+++ No 37 - Ocelot/src/services/GamerService/Controllers/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GamerService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly ILogger<PlayerController> _logger;
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+        }
+
+        /*
+            HTTP Get taleplerine karşılık verecek metodumuzdan geriye sembolik olarak bir Player nesnesi döndürüyoruz.
+            Player/19 gibi gelen taleplere cevap verecek
+        */
+        [HttpGet("{id}")]
+        public Player Get(string id)
+        {
+            return new Player
+            {
+                Id = id,
+                Fullname = "Megen Enever",
+                Level = 58,
+                Location = "Dublin"
+            };
+        }
+    }
+
+    public class Player
+    {
+        public string Id { get; set; }
+        public string Fullname { get; set; }
+        public int Level { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ No 37 - Ocelot/src/services/GamerService/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GamerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    // Farklı bir porttan yayın yapsın
+                    webBuilder.UseStartup<Startup>().UseUrls("http://localhost:6501");
+                });
+    }
+}
