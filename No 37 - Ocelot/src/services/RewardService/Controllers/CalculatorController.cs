dfa2249563073f8c4ab9d0c7d1fada0cba240147
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-09 11:19:31 +0300

    Dockerize edeceğim servisi ekledim

---
+++ No 37 - Ocelot/src/services/RewardService/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RewardService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private static readonly string[] topics = new[]
+        {
+            "1000 Free Spell"
+            , "10 Free Coin"
+            , "30 Days Free Trail"
+            , "Gold Ticket"
+            ,"Legendary Tournemant Pass"
+            ,"1000 Free Retro Game"
+            ,"One Day All Games Free"
+        };
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Reward> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 3).Select(index => new Reward
+            {
+                Duration = rng.Next(7, 60),
+                Description = topics[rng.Next(topics.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+    public class Reward{
+        public int Duration { get; set; }
+        public string Description { get; set; }
+    }
+}
