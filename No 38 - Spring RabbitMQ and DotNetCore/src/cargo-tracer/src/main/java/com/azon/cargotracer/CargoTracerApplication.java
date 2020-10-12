b3efc911aca72e41fe36e840ebbba11c2e75ab5f
buraksenyurt <burakselimsenyurt@gmail.com>
2020-10-12 21:22:25 +0300

    Rabbitmq bağlantı bilgileri konfigurasyona çekildi

--- No 38 - Spring RabbitMQ and DotNetCore/src/cargo-tracer/src/main/java/com/azon/cargotracer/CargoTracerApplication.java
+++ No 38 - Spring RabbitMQ and DotNetCore/src/cargo-tracer/src/main/java/com/azon/cargotracer/CargoTracerApplication.java
@@ -7,8 +7,10 @@ import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration // RabbitMQ sunucu adı, bağlantı portu, kullanıcı adı vb bilgileri application.properties'ten almasını söyledik
 public class CargoTracerApplication {
 
 	@Bean
